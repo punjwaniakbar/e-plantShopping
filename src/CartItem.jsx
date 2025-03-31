e9fb01a38e254b2b477a17f963ceda7d46dbe2e6
Akbar Punjwani <akbarpunjwani@yahoo.com>
2025-03-31 16:41:39 -0400

    Task 3: CartItems component completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total = 0;
+
+    cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1)); // Convert cost to number
+        total += quantity * cost; // Calculate the total
+    });
  
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>1){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    else{
+        dispatch(removeItem({name: item.name}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
   };
 
   return (
