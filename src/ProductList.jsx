8d640e82a4dfd5bfc8d0b2e297f4d7a79db9165a
Akbar Punjwani <akbarpunjwani@yahoo.com>
2025-03-31 16:55:14 -0400

    Minor changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -294,6 +294,8 @@ function ProductList({ onHomeClick }) {
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
                                 {/*Similarly like the above plant.name show other details like description and cost*/}
+                                <div className="product-description">{plant.description}</div>
+                                <div className="product-cost">{plant.cost}</div>
                                 <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                             </div>
                             ))}
